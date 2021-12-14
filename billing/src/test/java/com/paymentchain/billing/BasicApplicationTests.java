7b76020baab33699743b0143f5f02e20e76a1773
Freddy Mora <freddy.mora_8@hotmail.com>
2021-12-14 14:02:30 -0500

    new port and image docker

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -67,7 +67,7 @@ public class BasicApplicationTests {
     }
 
     /**
-     * Test call of create method, integrated with sonar utl.
+     * Test call of create method, on  weblayer fmo ult slack update java version ult ok.
      */
     @Test
     public void testCreate() throws Exception {
